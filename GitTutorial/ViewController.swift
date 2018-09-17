7961e1b0624e5bf6eb0a469c05c42bd1404c676d
He Tian <tian@hes-macbook-pro-2.local>
2018-09-16 21:24:57 -0400

    test1

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let message = "Hello Git!"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
